545b3addc8f4bc28437adcb10379880d165829f5
Moha <mohamedhalouy@gmail.com>
2023-04-12 01:02:15 +0200

    prueba finalizada

--- js/index.js
+++ js/index.js
@@ -1,25 +1,35 @@
+const input = document.getElementById('inputNum');
+
 const convertBinary = num => num.toString(2);
 
-const contMaxZero = (num) => {
-    const numBinary = convertBinary(num)
-    const numBinaryStr = numBinary.toString();
-    const digits = numBinaryStr.split("");
+// Función principal para contar los ceros binarios.
+const contMaxZero = () => {
+    const num = parseInt(document.getElementById('inputNum').value)
+    const numBinary = convertBinary(num);
+    const digits = Array.from(numBinary);
 
     let countZeros = 0;
     let numMaxZeros = 0;
 
+    // Se recorre el array de dígitos y se cuentan los ceros consecutivos.
     for (const digit of digits) {
         if (parseInt(digit) === 1) {
+            // Si se encuentra un 1, se actualiza el número máximo de ceros consecutivos.
             numMaxZeros = Math.max(numMaxZeros, countZeros);
             countZeros = 0;
         } else {
+            // Si se encuentra un 0, se incrementa el contador de ceros consecutivos.
             countZeros++
         }
     }
 
+    document.getElementById('converted-data').textContent = numMaxZeros;
+};
 
-    return numMaxZeros;
-}
+input.addEventListener('input', contMaxZero);
 
-
-console.log(contMaxZero(545))
+input.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13) {
+        contMaxZero();
+    }
+});
